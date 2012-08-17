52499887c18592f51f47e370f20f50716e8baf42
高尾 宏治 <kouji@dhcp105.priv.netlab.jp>
2012-08-17 10:57:57 +0900

    rails new hello
    以下のコマンドを実行した。
    $ rails new hello
    $ git add hello
    $ git commit

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'c968cdb1c40b4aa3f015f8d72984b80b8ed4cda2fcfc27caa8441ae79656f8423e493c908bffa8f89872dac346a15ac345a1725f8bf18b316e7ff13e2fca701f'
