564108aad60ff9ffcced36149fe8171ee040345c
高尾 宏治 <kouji@dhcp105.priv.netlab.jp>
2012-08-17 14:18:03 +0900

    カウントアップをコントローラに移動

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @counter = Counter.first
+    Counter.increment_counter(:count, @counter.id)
+    @counter.reload
   end
 
   def contact
